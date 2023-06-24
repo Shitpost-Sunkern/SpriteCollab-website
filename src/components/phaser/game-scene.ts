20407f3d7625146f45cf45bb9bc61f3122c6cbff
Shitpost Sunkern <shitpostsunkern@gmail.com>
2023-06-24 16:39:42 -0400

    Fix shadow alignment

--- src/components/phaser/game-scene.ts
+++ src/components/phaser/game-scene.ts
@@ -62,7 +62,7 @@ export default class GameScene extends Phaser.Scene {
     const scaleFactor = this.metadata?.FrameHeight ?? 0 < 100 ? 2 : 1;
     this.add.image(100, 100, "small-ba").setScale(scaleFactor, scaleFactor);
     this.add
-      .sprite(100, 110, `${this.action}-${AnimationType.SHADOW}`)
+      .sprite(100, 105, `${this.action}-${AnimationType.SHADOW}`)
       .setScale(scaleFactor, scaleFactor)
       .setTintFill(0xffffff)
       .play(AnimationType.SHADOW);
